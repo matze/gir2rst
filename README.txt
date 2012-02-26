aed9c0b524fcda58b64272e33ecf00ff85f6bfba
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-02-26 21:33:27 +0100

    Share same text for README and description

--- README.txt
+++ README.txt
@@ -1,7 +1,27 @@
-=======
 gir2rst
 =======
 
-gir2rst creates Sphinx documentation from GObject-Introspection files.
+gir2rst provides a package and command-line tool to create Sphinx documentation
+from GObject-Introspection files.
 
+
+Installation
+------------
+
+Fetch the latest version from Github ::
+
+    git clone git://github.com/matze/gir2rst.git
+
+and install with::
+
+    python setup.py install
+
+
+Usage
+-----
+
+The command line tool ``gir2rst`` will parse the provided .gir files and output
+the resulting Sphinx documentation on stdout::
+
+    gir2rst /home/user/path/to/gir/Library-x.y.gir
 
--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from distutils.core import setup
 
-setup(name='gir2rst', 
-        version='0.1', 
-        scripts=['bin/gir2rst'],
-        packages=['gir2rst'],
-        author='Matthias Vogelgesang',
-        author_email='matthias.vogelgesang@gmail.com',
-        url='http://github.com/matze/gir2rst',
-        license='GPL v3',
-        description='Generate ReST-based Sphinx documentation from GIR files',)
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup"
+]
+
+with open('README.txt') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    name='gir2rst', 
+    version='0.1', 
+    packages=['gir2rst'],
+    scripts=['bin/gir2rst'],
+    author='Matthias Vogelgesang',
+    author_email='matthias.vogelgesang@gmail.com',
+    url='http://github.com/matze/gir2rst',
+    keywords=['rst', 'gir'],
+    license='GPL v3',
+    classifiers=CLASSIFIERS,
+    description='ReST-based Sphinx documentation from GIR files',
+    long_description=LONG_DESCRIPTION
+)
