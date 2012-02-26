1da266092ce8027a9721b07118cb0ef2f548fc80
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2012-02-26 21:55:09 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.rst') as fp:
 
 setup(
     name='gir2rst', 
-    version='0.1', 
+    version='0.2', 
     packages=['gir2rst'],
     scripts=['bin/gir2rst'],
     author='Matthias Vogelgesang',
